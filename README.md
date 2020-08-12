ba3c850b7115567d673359d45c94424f0750b767
saiki <saiki@Sai_Kiran.fios-router.home>
2020-08-12 11:40:04 -0500

    Find the members remaining a room

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 Follow programs are there in repo.
 
-1)GetAllEvenNumbersFirst - This program will sort/swap the elements so that all even numbers will be moved to start of array
+1) GetAllEvenNumbersFirst - This program will sort/swap the elements so that all even numbers will be moved to start of array
 	Ex:- [1,4,3,10] after our execution it should be [4,10,3,1]
 	
 2) This class to group the anagrams Using Java8 and also before Java8
 	Ex:- "eat" and "tea" are anagrams
 	
-3) Remove Suplicate Characters in String using Java8 and Before Java8
+3) Remove Duplicate Characters in String using Java8 and Before Java8
 	EX:- "create eat" output:-"creat"
+
+4) Find the members remaining a room. where input is a list of strings with employeId and RoomEntryIndicator.
+		 "<"-- exited out of the room, ">"-- entered in to the room.
+	EX:-["1,>","2,>","3,<"] -- in this employee 1 and 2 entered into the room and employee 3 exited the room. 	 
---
+++ src/main/java/com/java/eight/FindMemebersLeftInRoom.java
@@ -0,0 +1,69 @@
+package com.java.eight;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class FindMemebersLeftInRoom {
+
+	public static void main(String[] args) {
+		//in this comma separated values treat number as employee id and '<' and '>' as in and out to the room
+		List<String> membersInList = Arrays.asList("1, >","2, >", "3, >","1, <","5, >","2, >");
+		Predicate<String> pred = t ->  t != null && (t.contains(RoomEntry.OUT.getEntry()) || t.contains(RoomEntry.In.getEntry()));
+		
+		Map<Object, Object> remMembers =
+				membersInList.stream()
+					 .filter(pred)
+					 .map(s ->s.split(","))
+					 .map(strArr -> Arrays.asList(strArr))
+					 .filter(strArr -> strArr.size() ==2)
+					 .collect(Collectors.toMap(s -> getKey(s), s->getValue(s), extracted()));
+		
+		System.out.println(remMembers.entrySet().stream().filter(entry -> entry.getValue().equals(RoomEntry.In.getEntry())).map(Entry::getKey).collect(Collectors.toList()));
+	}
+
+	private static BinaryOperator<Object> extracted() {
+		return (prev, curr)-> {
+			 String prevValue = (String) prev;
+			 String currValue = (String) curr;
+			 String returnString = "";
+			 if( prevValue.equals(currValue) && prevValue.equals(RoomEntry.In.getEntry())){
+				 returnString = prevValue;
+			 } else if( prevValue.equals(currValue) && prevValue.equals(RoomEntry.OUT.getEntry())){
+				 returnString = prevValue;
+			}else {
+				 returnString = currValue;
+			}
+			 return returnString;
+		 };
+	}
+
+	private static String getValue(List<String> s) {
+		String inputValueStr = s.get(1).trim();
+		String strLitIn = RoomEntry.OUT.getEntry();
+		String strLitOut = RoomEntry.In.getEntry();
+		return inputValueStr.equals(strLitIn) ? inputValueStr : (inputValueStr.equals(strLitOut)? inputValueStr : "");
+	}
+
+	private static String getKey(List<String> s) {
+		return s.get(0);
+	}
+
+}
+
+enum RoomEntry{
+	In(">"),OUT("<");
+
+	public String getEntry() {
+		return this.entry;
+	}
+	private String entry;
+
+	RoomEntry(String string) {
+		this.entry = string;
+	}
+}
